0b7c3ff406df1b74e8b04d261418f8d666721fa0
Webb-balson <balsongorai@gmail.com>
2019-01-30 02:44:18 +0530

    Added form and view,model,template.

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Student(models.Model):
+    usn = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    semester = models.IntegerField()
+    parentName = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    complain = models.TextField()
+
